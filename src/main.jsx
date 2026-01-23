bc3af5a17851e256e0f785964c2894eac7921a5c
Marco Gurrata <marco.gurrata@stevejobs.academy>
2026-01-23 17:27:10 +0100

    fix: app router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/React_jump_higher">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
